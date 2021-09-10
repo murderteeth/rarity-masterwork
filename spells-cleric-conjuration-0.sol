1191d0768a39f2cd5f3012959d9eb7c2e9d6e167
Ismael <ismaelJimenez@users.noreply.github.com>
2021-09-10 16:38:04 +0200

    Add cleric spells

--- spells-cleric-conjuration-0.sol
+++ spells-cleric-conjuration-0.sol
@@ -13,6 +13,7 @@ contract codex {
         bool verbal,
         bool somatic,
         bool focus,
+        bool divine_focus,
         uint xp_cost,
         uint time,
         uint range,
@@ -22,9 +23,9 @@ contract codex {
         bool spell_resistance,
         string memory description
     ) {
-        if (_id == 30) {
+        if (_id == 2) {
             return create_water();
-        } else if (_id == 31) {
+        } else if (_id == 3) {
             return cure_minor_wounds();
         }
     }
@@ -35,6 +36,7 @@ contract codex {
         bool verbal,
         bool somatic,
         bool focus,
+        bool divine_focus,
         uint xp_cost,
         uint time,
         uint range,
@@ -44,11 +46,12 @@ contract codex {
         bool spell_resistance,
         string memory description
     ) {
-        id = 30;
+        id = 2;
         name = "Create Water";
         verbal = true;
         somatic = true;
         focus = false;
+        divine_focus = false;
         xp_cost = 0;
         time = 1;
         range = 2;
@@ -56,7 +59,7 @@ contract codex {
         saving_throw_type = 0;
         saving_throw_effect = 0;
         spell_resistance = false;
-        description = "This spell generates wholesome, drinkable water, just like clean rain water. Water can be created in an area as small as will actually contain the liquid, or in an area three times as large—possibly creating a downpour or filling many small receptacles.";
+        description = "This spell generates wholesome, drinkable water, just like clean rain water. Water can be created in an area as small as will actually contain the liquid, or in an area three times as large, possibly creating a downpour or filling many small receptacles. Note: Conjuration spells cant create substances or objects within a creature. Water weighs about 8 pounds per gallon. One cubic foot of water contains roughly 8 gallons and weighs about 60 pounds.";
     }
 
     function cure_minor_wounds() public pure returns (
@@ -65,6 +68,7 @@ contract codex {
         bool verbal,
         bool somatic,
         bool focus,
+        bool divine_focus,
         uint xp_cost,
         uint time,
         uint range,
@@ -74,19 +78,19 @@ contract codex {
         bool spell_resistance,
         string memory description
     ) {
-        id = 31;
+        id = 3;
         name = "Cure Minor Wounds";
         verbal = true;
-        somatic = false;
+        somatic = true;
         focus = false;
+        divine_focus = false;
         xp_cost = 0;
         time = 1;
         range = 1;
         duration = 0;
-        saving_throw_type = 0;
-        saving_throw_effect = 0;
+        saving_throw_type = 3;
+        saving_throw_effect = 2;
         spell_resistance = true;
-        description = "When laying your hand upon a living creature, you channel positive energy that cures 1 point of damage. Since undead are powered by negative energy, this spell deals damage to them instead of curing their wounds";
+        description = "This spell functions like cure light wounds, except that it cures only 1 point of damage.";
     }
-    
 }
