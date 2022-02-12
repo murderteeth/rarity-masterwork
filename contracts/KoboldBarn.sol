18a76ba4a613b7c03e42ea12e7ddbd9a008af111
Zach Gohr <zachary.gohr@gmail.com>
2022-02-12 11:08:49 -0600

    Messing around with the library architecture a bit

--- contracts/KoboldBarn.sol
+++ contracts/KoboldBarn.sol
@@ -2,23 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "./utils/RaritySkillCheck.sol";
-import "./utils/RarityOwnership.sol";
-
-import "./core/interfaces/IRarity.sol";
-import "./core/interfaces/IMaterials.sol";
-import "./core/interfaces/IAttributes.sol";
+import "./utils/Rarity.sol";
 
 contract KoboldBarn is ERC721Enumerable {
-    using Counters for Counters.Counter;
-    Counters.Counter private nextTokenId;
-
-    IMaterials private constant MATERIALS =
-        IMaterials(0x2A0F1cB17680161cF255348dDFDeE94ea8Ca196A);
-    IAttributes private constant ATTRIBUTES =
-        IAttributes(0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1);
+    uint256 public nextToken = 1;
 
     // Helper constants
     uint256 private constant DAY = 1 days;
@@ -27,7 +16,7 @@ contract KoboldBarn is ERC721Enumerable {
 
     struct Kobold {
         uint8 health;
-        uint256 koboldId;
+        uint256 summonerId;
         uint8 summonerHealth;
         uint256 armorId;
         uint256 weaponId;
@@ -35,7 +24,8 @@ contract KoboldBarn is ERC721Enumerable {
         bool masterwork;
     }
 
-    mapping(uint256 => Kobold) public kobolds; // summonerId => Kobold - the kobold the summoner is fighting
+    mapping(uint256 => uint256) public latestFight; // summonerId => koboldId
+    mapping(uint256 => Kobold) public kobolds;
 
     // Attack to start or continue a fight
     function attack(uint256 summonerId)
@@ -43,7 +33,8 @@ contract KoboldBarn is ERC721Enumerable {
         approvedForSummoner(summonerId)
     {
         // Get the summoner's latest kobold
-        Kobold memory latestKobold = kobolds[summonerId];
+        uint256 koboldId = latestFight[summonerId];
+        Kobold memory latestKobold = kobolds[koboldId];
 
         if (latestKobold.health == 0 || latestKobold.summonerHealth == 0) {
             // Pick a fight with a new Kobold
@@ -51,13 +42,21 @@ contract KoboldBarn is ERC721Enumerable {
 
             _fight(newKobold, summonerId);
         } else {
-            uint256 koboldId = latestKobold.koboldId;
             require(_isApprovedOrOwner(_msgSender(), koboldId), "!kobold");
             // Summoners can only attack once per day
             require(block.timestamp > latestKobold.lastTurn + DAY, "!turn");
 
             _fight(latestKobold, summonerId);
         }
+    }
+
+    function summonerOf(uint256 koboldId) public view returns (uint256) {
+        return kobolds[koboldId].summonerId;
+    }
+
+    function isWon(uint256 koboldId) public pure returns (bool) {
+        // TODO
+        return true;
     }
 
     function _fight(Kobold memory kobold, uint256 summonerId) internal {
@@ -67,22 +66,21 @@ contract KoboldBarn is ERC721Enumerable {
     }
 
     function _startFight(uint256 summonerId) internal returns (Kobold memory) {
-        uint256 newTokenId = nextTokenId.current();
+        uint256 newTokenId = nextToken;
         _safeMint(_msgSender(), newTokenId);
 
         // Sneak attack means the kobold could start with 9 health
-        kobolds[summonerId].health = _koboldStartingHealth(summonerId);
-        kobolds[summonerId].koboldId = newTokenId;
+        latestFight[summonerId] = newTokenId;
+        kobolds[newTokenId].health = _koboldStartingHealth(summonerId);
+        kobolds[newTokenId].summonerId = summonerId;
 
-        kobolds[summonerId].summonerHealth = _summonerStartingHealth(
-            summonerId
-        );
+        kobolds[newTokenId].summonerHealth = Rarity.health(summonerId);
 
-        kobolds[summonerId].lastTurn = block.timestamp;
+        kobolds[newTokenId].lastTurn = block.timestamp;
 
-        nextTokenId.increment();
+        nextToken += 1;
 
-        return kobolds[summonerId];
+        return kobolds[newTokenId];
     }
 
     function _koboldStartingHealth(uint256 summonerId)
@@ -98,22 +96,10 @@ contract KoboldBarn is ERC721Enumerable {
         }
     }
 
-    function _summonerStartingHealth(uint256 summonerId)
-        internal
-        view
-        returns (uint8)
-    {
-        uint256 _level = RarityOwnership.rm().level(summonerId);
-        uint256 _class = RarityOwnership.rm().class(summonerId);
-        (, , uint32 _const, , , ) = ATTRIBUTES.ability_scores(summonerId);
-        return
-            uint8(MATERIALS.health_by_class_and_level(_class, _level, _const));
-    }
-
     // Modifiers
 
     modifier approvedForSummoner(uint256 summonerId) {
-        if (RarityOwnership._isApprovedOrOwnerOfSummoner(summonerId)) {
+        if (Rarity._isApprovedOrOwnerOfSummoner(summonerId)) {
             revert("!approved");
         } else {
             _;
--- contracts/RarityKoboldSalvage.sol
+++ contracts/RarityKoboldSalvage.sol
@@ -4,37 +4,37 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "./utils/RarityOwnership.sol";
+import "./utils/Rarity.sol";
 
 interface IKoboldBarn {
-    function kobolds(uint256)
-        external
-        view
-        returns (
-            uint8 health,
-            uint256 koboldId,
-            uint8 summonerHealth,
-            uint256 armorId,
-            uint256 weaponId,
-            uint256 lastTurn,
-            bool masterwork
-        );
+    function summonerOf(uint256) external view returns (uint256);
+
+    function isWon(uint256) external view returns (bool);
 }
 
 contract RarityKoboldSalvage is ERC20 {
+    IKoboldBarn private koboldBarn;
+
     mapping(uint256 => bool) public claimedKobolds;
 
-    constructor() ERC20("Rarity Kobold Salvage", "RKS") {}
+    constructor(IKoboldBarn barn) ERC20("Rarity Kobold Salvage", "RKS") {
+        koboldBarn = barn;
+    }
 
     function claim(uint256 summonerId, uint256 koboldId)
         external
         approvedForSummoner(summonerId)
-    {}
+    {
+        require(claimedKobolds[koboldId] == false, "!claimed");
+        require(koboldBarn.summonerOf(koboldId) == summonerId, "!summoner");
+        require(koboldBarn.isWon(koboldId), "!won");
+        _mint(_msgSender(), 1);
+    }
 
     // Modifiers
 
     modifier approvedForSummoner(uint256 summonerId) {
-        if (RarityOwnership._isApprovedOrOwnerOfSummoner(summonerId)) {
+        if (Rarity._isApprovedOrOwnerOfSummoner(summonerId)) {
             revert("!approved");
         } else {
             _;
