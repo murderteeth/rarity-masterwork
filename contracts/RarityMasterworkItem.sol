f4429336bc4a26523fb6c888b600f28d3eb42d9b
murderteeth <murdertxxth@gmail.com>
2022-02-18 13:08:34 -1000

    ⚒ Add masterwork tools codex

--- contracts/RarityMasterworkItem.sol
+++ contracts/RarityMasterworkItem.sol
@@ -19,8 +19,33 @@ interface IProjects {
         uint32 started;
         uint32 completed;
     }
+}
 
-    function projects(uint256) external view returns (Project memory);
+interface IToolsCodex {
+  struct effects_struct {
+    int[36] skill_bonus;
+  }
+
+  function item_by_id(uint _id) external pure returns(
+    uint8 id,
+    uint cost,
+    uint weight,
+    string memory name,
+    string memory description,
+    effects_struct memory effects
+  );
+}
+
+interface IEffects {
+  function skill_bonus(uint token, uint8 skill) external view returns (int);
+}
+
+contract RarityMasterworkItem is ERC721Enumerable, IEffects {
+  uint public nextToken = 1;
+  IRarity rarity = IRarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
+  IProjects projects = IProjects(0x000000000000000000000000000000000000dEaD);
+  IToolsCodex toolsCodex = IToolsCodex(0x000000000000000000000000000000000000dEaD);
+  event Claimed(address indexed owner, uint token, uint projectToken);
 
     function ownerOf(uint256) external view returns (uint256);
 }
@@ -62,16 +87,26 @@ contract RarityMasterworkItem is ERC721Enumerable {
         );
         claimed[projectToken] = true;
         emit Claimed(msg.sender, nextToken, projectToken);
-
         nextToken++;
     }
 
-    function itemOfOwnerByIndex(address owner, uint256 index)
-        external
-        view
-        returns (Item memory)
-    {
-        uint256 token = tokenOfOwnerByIndex(owner, index);
-        return items[token];
-    }
+  function skill_bonus(uint token, uint8 skill) override external view returns (int) {
+    Item memory item = items[token];
+    (,,,,,IToolsCodex.effects_struct memory effects) = toolsCodex.item_by_id(item.itemType);
+    return effects.skill_bonus[skill];
+  }
+
+  function itemOfOwnerByIndex(address owner, uint index) external view returns (Item memory) {
+    uint token = tokenOfOwnerByIndex(owner, index);
+    return items[token];
+  }
+
+  function itemOfOwnerByIndex(address owner, uint256 index)
+    external
+    view
+    returns (Item memory)
+  {
+    uint256 token = tokenOfOwnerByIndex(owner, index);
+    return items[token];
+  }
 }
