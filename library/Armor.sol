87d3047a5e4706ec7dae8748ea853fe2227fb648
murderteeth <murdertxxth@gmail.com>
2022-03-19 22:40:12 -1000

    Fix armor logic

--- library/Armor.sol
+++ library/Armor.sol
@@ -20,8 +20,7 @@ library Armor {
     (, uint256 itemType, , ) = IRarityCommonCrafting(armorContract).items(armor);
     (, , , , uint256 armorBonus, uint256 maxDexBonus, , , , ) = ARMOR_CODEX.item_by_id(itemType);
 
-    int8 dexModifier = Attributes.dexterityModifier(summoner) 
-      + proficiencyBonus(summoner, armor, armorContract);
+    int8 dexModifier = Attributes.dexterityModifier(summoner);
 
     int8 result = 10 + int8(uint8(armorBonus));
     if (dexModifier > int256(maxDexBonus)) {
@@ -30,24 +29,5 @@ library Armor {
       result += dexModifier;
     }
     return uint8(result);
-  }
-
-  function proficiencyBonus(
-    uint256 summoner,
-    uint256 armor,
-    address armorContract
-  ) public view returns (int8) {
-    if (armorContract == address(0)) {
-      return 0;
-    }
-
-    (, uint256 itemType,,) = IRarityCommonCrafting(armorContract).items(armor);
-    (,, uint256 proficiency,,,, int256 penalty,,,) = ARMOR_CODEX.item_by_id(itemType);
-
-    if (!Proficiency.isProficientWithArmor(summoner, proficiency, itemType)) {
-      return int8(penalty);
-    } else {
-      return 0;
-    }
   }
 }
