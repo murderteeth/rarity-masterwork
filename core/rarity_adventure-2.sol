fdec5545ec85b5c18dceccca0ea8b77806b38cf5
murderteeth <murdertxxth@gmail.com>
2022-03-20 17:36:25 -1000

    Sense the farmer's motives

--- core/rarity_adventure-2.sol
+++ core/rarity_adventure-2.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../interfaces/core/IRarity.sol";
 import "../library/ForSummoners.sol";
+import "../library/SkillCheck.sol";
 
 contract rarity_adventure_2 is ERC721Enumerable, ForSummoners {
   uint public next_token = 1;
   uint private constant DAY = 1 days;
-  uint8 public constant SENSE_MOTIVE_DC = 15;
+  uint public constant SENSE_MOTIVE_XP_COST = 347e15;  // ~ 2 minutes of xp
+  uint8 public constant FARMERS_KEY_DC = 20;
 
   int8 public constant MONSTER_INITIATIVE_BONUS = 1;
   uint8 public constant MONSTER_AC = 15;
@@ -16,9 +19,14 @@ contract rarity_adventure_2 is ERC721Enumerable, ForSummoners {
   uint8 public constant MONSTER_HIT_DICE_BONUS = 4;
   uint8 public constant MONSTER_DEX = 13;
 
+  IRarity public constant RARITY = IRarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
+
   constructor() ERC721("Rarity Adventure (II)", "Adventure (II)") {}
 
+  event SenseFarmersMotive(uint token, uint8 roll, uint8 score);
+
   struct Adventure {
+    bool farmersKey;
     uint summoner;
     uint startedOn;
     uint endedOn;
@@ -36,20 +44,31 @@ contract rarity_adventure_2 is ERC721Enumerable, ForSummoners {
     next_token += 1;
   }
 
-  function isApprovedOrOwnerOfAdventure(uint adventure) public view returns (bool) {
-    return getApproved(adventure) == msg.sender
-      || ownerOf(adventure) == msg.sender
-      || isApprovedForAll(ownerOf(adventure), msg.sender);
+  function sense_farmers_motive(uint token) public approvedForAdventure(token) {
+    Adventure storage adventure = adventures[token];
+    require(adventure.endedOn == 0, "adventure.endedOn != 0");
+    (uint8 roll, uint8 score) = SkillCheck.senseMotive(adventure.summoner);
+    if(!adventure.farmersKey && score >= FARMERS_KEY_DC) {
+      adventure.farmersKey = true;
+    }
+    RARITY.spend_xp(adventure.summoner, SENSE_MOTIVE_XP_COST);
+    emit SenseFarmersMotive(token, roll, score);
   }
 
-  modifier approvedForAdventure(uint adventure) {
-    if (isApprovedOrOwnerOfAdventure(adventure)) {
+  // TODO: tokenURI
+
+  function isApprovedOrOwnerOfAdventure(uint token) public view returns (bool) {
+    return getApproved(token) == msg.sender
+      || ownerOf(token) == msg.sender
+      || isApprovedForAll(ownerOf(token), msg.sender);
+  }
+
+  modifier approvedForAdventure(uint token) {
+    if (isApprovedOrOwnerOfAdventure(token)) {
       _;
     } else {
       revert("!approvedForAdventure");
     }
   }
-
-  // TODO: tokenURI
 
 }
