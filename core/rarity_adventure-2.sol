f1d01d53976f8138c71f3abe8e21f516b66cf86b
murderteeth <murdertxxth@gmail.com>
2022-03-24 12:08:06 -1000

    Only sense the farmer's motives once per adventure

--- core/rarity_adventure-2.sol
+++ core/rarity_adventure-2.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "../interfaces/core/IRarity.sol";
 import "../library/ForSummoners.sol";
 import "../library/SkillCheck.sol";
 
@@ -19,39 +18,39 @@ contract rarity_adventure_2 is ERC721Enumerable, ForSummoners {
   uint8 public constant MONSTER_HIT_DICE_BONUS = 4;
   uint8 public constant MONSTER_DEX = 13;
 
-  IRarity public constant RARITY = IRarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
-
   constructor() ERC721("Rarity Adventure (II)", "Adventure (II)") {}
 
   event SenseFarmersMotive(uint token, uint8 roll, uint8 score);
 
   struct Adventure {
-    bool farmersKey;
     uint summoner;
-    uint startedOn;
-    uint endedOn;
+    uint started;
+    uint ended;
+    bool farmers_bluff_checked;
+    bool farmers_key;
   }
 
   mapping(uint => Adventure) public adventures;
-  mapping(uint => uint) public activeAdventures;
+  mapping(uint => uint) public active_adventures;
 
   function start(uint summoner) public approvedForSummoner(summoner) {
-    require(activeAdventures[summoner] == 0, "activeAdventures[summoner] != 0");
+    require(active_adventures[summoner] == 0, "active_adventures[summoner] != 0");
     adventures[next_token].summoner = summoner;
-    adventures[next_token].startedOn = block.timestamp;
-    activeAdventures[summoner] = next_token;
+    adventures[next_token].started = block.timestamp;
+    active_adventures[summoner] = next_token;
     _safeMint(_msgSender(), next_token);
     next_token += 1;
   }
 
   function sense_farmers_motive(uint token) public approvedForAdventure(token) {
     Adventure storage adventure = adventures[token];
-    require(adventure.endedOn == 0, "adventure.endedOn != 0");
+    require(adventure.ended == 0, "ended != 0");
+    require(!adventure.farmers_bluff_checked, "farmers_bluff_checked == true");
+ 
     (uint8 roll, uint8 score) = SkillCheck.senseMotive(adventure.summoner);
-    if(!adventure.farmersKey && score >= FARMERS_KEY_DC) {
-      adventure.farmersKey = true;
-    }
-    RARITY.spend_xp(adventure.summoner, SENSE_MOTIVE_XP_COST);
+    adventure.farmers_key = score >= FARMERS_KEY_DC;
+    adventure.farmers_bluff_checked = true;
+ 
     emit SenseFarmersMotive(token, roll, score);
   }
 
