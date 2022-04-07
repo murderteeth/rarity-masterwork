c6f07b7f0304b7564d3fecf13cf1e5cf838d53b2
murderteeth <murdertxxth@gmail.com>
2022-04-07 10:20:34 -1000

    Give shields back!

--- core/rarity_adventure-2.sol
+++ core/rarity_adventure-2.sol
@@ -294,6 +294,11 @@ contract rarity_adventure_2 is ERC721Enumerable, IERC721Receiver, ForSummoners, 
       ICrafting(armor_slot.item_contract).safeTransferFrom(address(this), _msgSender(), armor_slot.item);
     }
 
+    EquipmentSlot memory shield_slot = equipment_slots[token][EQUIPMENT_TYPE_SHIELD];
+    if(shield_slot.item_contract != address(0)) {
+      ICrafting(shield_slot.item_contract).safeTransferFrom(address(this), _msgSender(), shield_slot.item);
+    }
+
     adventure.ended = uint64(block.timestamp);
   }
 
