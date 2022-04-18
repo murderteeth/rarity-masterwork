0ff87acaff40e2ab58bae61040b40255ff2a0f21
murderteeth <murdertxxth@gmail.com>
2022-04-17 20:50:09 -1000

    Add ac and damage bonuses for unarmed/unarmored monks

--- library/Summoner.sol
+++ library/Summoner.sol
@@ -34,6 +34,14 @@ library Summoner {
       if(shield_codex.max_dex_bonus < max_dex_bonus) max_dex_bonus = shield_codex.max_dex_bonus;
     }
 
+    if(armor_slot.item_contract == address(0) 
+      && shield_slot.item_contract == address(0)
+      && Rarity.class(summoner) == 6
+    ) {
+      result += int8(Attributes.wisdom_modifier(summoner))
+      + int8(int(Rarity.level(summoner) / 5));
+    }
+
     result = result + ((dex_modifier > int256(max_dex_bonus)) ? int8(uint8(max_dex_bonus)) : dex_modifier);
 
     return uint8(result);
@@ -109,11 +117,23 @@ library Summoner {
   ) {
     IWeapon.Weapon memory weapon_codex = get_weapon(weapon_slot);
 
+    if(weapon_codex.id == 0 && Rarity.class(summoner) == 6) {
+      uint level = Rarity.level(summoner);
+      weapon_codex.damage = level < 4 ? 6 
+      : level < 8 ? 8 
+      : level < 12 ? 10 
+      : level < 16 ? 12
+      : level < 20 ? 16
+      : 20;
+    }
+
     int8 attack_modifier = weapon_attack_modifier(summoner, weapon_codex.encumbrance)
     + armor_check_penalty(summoner, armor_slot.item, armor_slot.item_contract)
     + armor_check_penalty(summoner, shield_slot.item, shield_slot.item_contract);
 
-    if(!Proficiency.isProficientWithWeapon(summoner, weapon_codex.proficiency, weapon_codex.id)) {
+    if(weapon_codex.id != 0
+      && !Proficiency.isProficientWithWeapon(summoner, weapon_codex.proficiency, weapon_codex.id)
+    ) {
       attack_modifier -= 4;
     }
 
@@ -125,7 +145,7 @@ library Summoner {
 
     int8[4] memory attack_bonus = base_attack_bonus(summoner);
     for(uint i = 0; i < 4; i++) {
-      if(attack_bonus[i] > 0) {
+      if(i == 0 || attack_bonus[i] > 0) {
         Combat.pack_attack(
           attack_bonus[i] + attack_modifier, 
           int8(weapon_codex.critical_modifier), 
